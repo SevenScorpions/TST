6dae977f42d37b5595923a6c054064ab8b055ad6
SevenScorpions <huyhoathangbmt@gmail.com>
2024-02-29 16:28:16 +0700

    .

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import os
+import re
 import pandas as pd
 from openpyxl import load_workbook
 import shutil
@@ -9,15 +10,15 @@ def process_folder(new_data_folder_path,data_folder_path, output_df, audio_type,
     file_count = len([f for f in os.listdir(data_folder_path)])
 
     for filename in os.listdir(new_data_folder_path):
-        if filename.endswith(".mp3"):
+        if filename.endswith(".mp3") or filename.endswith(".m4a"):
             file_number = filename.split(".")[0]
             file_number = file_number.split("-")[0]
-            new_file_number = int(file_number) + file_count
-            new_filename = f"{audio_prefix}{new_file_number}.mp3"
+            new_file_number = int(file_number) + file_count + 20
+            new_filename = f"{audio_prefix}{new_file_number}.wav"
 
             # Read data from words.xlsx and sens.xlsx
             words_df = pd.read_excel(os.path.join('Data Detail', 'AUDIO WORD.xlsx'), header=None)
-            sens_df = pd.read_excel(os.path.join('Data Detail', 'AUDIO SEN.xlsx'), header=None)
+            sens_df = pd.read_excel(os.path.join('Data Detail', 'DATA Me Hue 29.2.xlsx'), header=None)
 
             # Get data from respective rows
             if audio_prefix == "audio_s":
@@ -26,6 +27,9 @@ def process_folder(new_data_folder_path,data_folder_path, output_df, audio_type,
             else:
                 row_2 = words_df.iloc[int(file_number) - 1, 2]
                 row_3 = words_df.iloc[int(file_number) - 1, 1]
+
+            # Remove "12.", "13.",... at the beginning of row_3
+            row_3 = re.sub(r'^\d+\.', '', row_3.strip())
 
             # Append data to the output DataFrame
             output_df = pd.concat([output_df, pd.DataFrame({"File": [new_filename], "Mnong Transcript": [row_2], "Transcript": [row_3]})])
@@ -37,14 +41,14 @@ def process_folder(new_data_folder_path,data_folder_path, output_df, audio_type,
             print("moved",file_path,"to",new_file_path)
 
             # Check if sheet 2 exists, otherwise create it
-            if "Sheet2" not in data_wb.sheetnames:
-                sheet2 = data_wb.create_sheet(title="Sheet2")
-                sheet2.append(["File", "Mnong Transcript", "Transcript"])
+            if "Sheet1" not in data_wb.sheetnames:
+                sheet1 = data_wb.create_sheet(title="Sheet1")
+                sheet1.append(["File", "Mnong Transcript", "Transcript"])
             else:
-                sheet2 = data_wb["Sheet2"]
+                sheet1 = data_wb["Sheet1"]
 
             # Add data to sheet 2 of the Excel file
-            sheet2.append([new_filename, row_2, row_3])
+            sheet1.append([new_filename, row_2, row_3])
 
     return output_df
 
